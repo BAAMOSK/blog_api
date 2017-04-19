4ef49aad3f284e02d7f55a14f55f08e9b5260375
Scylol <djd3dobbin@gmail.com>
2017-04-19 15:55:14 -0400

    added delete and put

--- server.js
+++ server.js
@@ -29,6 +29,45 @@ app.post('/blogposts', jsonParser, (req, res) => {
 	res.status(201).json(item);
 });
 
+//-------------------------------------------------------------------
+
+app.delete('/blogposts/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted shopping list item \`${req.params.id}\``);
+	res.status(204).end();
+});
+
+app.put('/blogposts/:id', jsonParser, (req, res)=> {
+	console.log(req.params);
+	console.log(req.body);
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	for(let i = 0; i < requiredFields.length; i++) {
+	  const field = requiredFields[i];
+		if(!(field in req.body)) {
+			let message = `You missing the ${field}`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+		if(req.params.id !== req.body.id) {
+			const message = (
+      		`Request path id (${req.params.id}) and request body id `
+      		`(${req.body.id}) must match`);
+    		console.error(message);
+    		return res.status(400).send(message);
+		}
+		console.log(`Updating recipe item \`${req.params.id}\``);
+		const updatedItem = BlogPosts.update({
+			id: req.params.id,
+			title: req.body.title,
+			content: req.body.content,
+			author: req.body.author,
+			publishDate: req.body.publishDate
+		})
+		res.status(204).json(updatedItem);
+});
+
+
 app.listen(3000, () => {console.log('heelo!')});
 
 
