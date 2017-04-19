6b43bc00cbfaedfb3245f175ec4c8ddb6bf005a0
BAAMOSK <bunth667@gmail.com>
2017-04-19 15:20:56 -0400

    added the post method

--- server.js
+++ server.js
@@ -2,18 +2,42 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const {BlogPosts} = require('./models');
-const port = 8080;
+const jsonParser = require('body-parser').json();
 
-BlogPosts.create({
-	title: 'The Hobbit',
-	content: 'This book is long.',
-	author: 'J.R.R. Tolkien',
-	publishDate: 1965
-});
+BlogPosts.create('The Hobbit', 'This book is long.', 'J.R.R. Tolkien', 1965);
 
 app.get('/blogposts', (req, res) => {
   res.json(BlogPosts.get());  				
 });
 
+app.post('/blogposts', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	for(let i = 0; i < requiredFields.length; i++) {
+	  const field = requiredFields[i];
+		if(!(field in req.body)) {
+			let message = `You missing the ${field}`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	let item = BlogPosts.create(
+		req.body.title,
+		req.body.content,
+		req.body.author,
+		req.body.publishDate
+	);
+	res.status(201).json(item);
+});
 
 app.listen(3000, () => {console.log('heelo!')});
+
+
+
+
+
+
+
+
+
+
+
