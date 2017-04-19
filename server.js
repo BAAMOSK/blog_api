d180f555b0a79a29bdff9603d8139be4ae1c8f96
BAAMOSK <bunth667@gmail.com>
2017-04-19 15:04:22 -0400

    added the get function

--- server.js
+++ server.js
@@ -4,4 +4,16 @@ const morgan = require('morgan');
 const {BlogPosts} = require('./models');
 const port = 8080;
 
+BlogPosts.create({
+	title: 'The Hobbit',
+	content: 'This book is long.',
+	author: 'J.R.R. Tolkien',
+	publishDate: 1965
+});
+
+app.get('/blogposts', (req, res) => {
+  res.json(BlogPosts.get());  				
+});
+
+
 app.listen(3000, () => {console.log('heelo!')});
